5d30094e17074b5437166996dbd8e57ca7ef20d4
Matthew Landauer <mlandauer@gmail.com>
2019-05-21 12:03:52 +1000

    Use the library

--- scraper.rb
+++ scraper.rb
@@ -6,7 +6,10 @@ puts "Getting data in year `" + ENV['MORPH_PERIOD'] + "`, changable via MORPH_PE
 
 base_url = "http://pathway.onkaparinga.sa.gov.au/ePathway/Production/Web/"
 
-agent = Mechanize.new
+scraper = EpathwayScraper::Scraper.new(
+  "http://pathway.onkaparinga.sa.gov.au/ePathway/Production"
+)
+agent = scraper.agent
 
 # get to the page I can enter DA search
 page = agent.get base_url + "GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP"
@@ -15,10 +18,7 @@ i = 1;
 error = 0
 cont = true
 while cont do
-  form = page.form
-  form.field_with(:name=>'ctl00$MainBodyContent$mGeneralEnquirySearchControl$mTabControl$ctl04$mFormattedNumberTextBox').value = i.to_s + '/' + ENV['MORPH_PERIOD']
-  button = form.button_with(:value => "Search")
-  list = form.click_button(button)
+  list = scraper.search_for_one_application(page, "#{i}/#{ENV['MORPH_PERIOD']}")
 
   table = list.search("table.ContentPanel")
   unless ( table.empty? )
@@ -34,9 +34,7 @@ while cont do
       'date_received'     => Date.parse(tr.search('span')[1].inner_text).to_s,
     }
 
-    puts "Saving record " + record['council_reference'] + ", " + record['address']
-#      puts record
-    ScraperWiki.save_sqlite(['council_reference'], record)
+    EpathwayScraper.save(record)
   else
     error += 1
   end
