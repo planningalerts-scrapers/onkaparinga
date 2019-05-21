17ed14a724a84af7c6868dda887057224e3175b0
Matthew Landauer <mlandauer@gmail.com>
2019-05-21 12:24:29 +1000

    Simplify a little

--- scraper.rb
+++ scraper.rb
@@ -4,20 +4,17 @@ ENV['MORPH_PERIOD'] ||= DateTime.now.year.to_s
 
 puts "Getting data in year `" + ENV['MORPH_PERIOD'] + "`, changable via MORPH_PERIOD environment"
 
-base_url = "http://pathway.onkaparinga.sa.gov.au/ePathway/Production/Web/"
-
 scraper = EpathwayScraper::Scraper.new(
   "http://pathway.onkaparinga.sa.gov.au/ePathway/Production"
 )
 agent = scraper.agent
 
 # get to the page I can enter DA search
-page = agent.get base_url + "GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP"
+page = agent.get(scraper.base_url)
 
 i = 1;
 error = 0
-cont = true
-while cont do
+while error < 10 do
   list = scraper.search_for_one_application(page, "#{i}/#{ENV['MORPH_PERIOD']}")
 
   count = 0
@@ -33,7 +30,4 @@ while cont do
 
   # increase i value and scan the next DA
   i += 1
-  if error == 10
-    cont = false
-  end
 end
