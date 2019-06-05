d5c94e9199a84651dc0c5185c3a4d104dac36030
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 12:06:55 +1000

    Upgrade to latest version of the gem and add missing state to the end of the address

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,6 @@
 require "epathway_scraper"
 
-ENV['MORPH_PERIOD'] ||= DateTime.now.year.to_s
-year = ENV['MORPH_PERIOD'].to_i
-puts "Getting data in year `#{year}`, changable via MORPH_PERIOD environment"
-
 EpathwayScraper.scrape_and_save(
   "http://pathway.onkaparinga.sa.gov.au/ePathway/Production",
-  list_type: :all_year, year: year
+  list_type: :all_this_year, state: "SA"
 )
