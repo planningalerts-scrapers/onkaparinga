66131998f784e5ca5713f727c60aff5d00977c56
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 13:54:00 +1000

    Update to latest version of gem

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,3 @@
 require "epathway_scraper"
 
-EpathwayScraper.scrape_and_save(
-  "http://pathway.onkaparinga.sa.gov.au/ePathway/Production",
-  list_type: :all_this_year, state: "SA"
-)
+EpathwayScraper.scrape_and_save_authority(:onkaparinga)
